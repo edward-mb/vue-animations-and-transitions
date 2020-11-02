d4e329c1e4785103fef7a16b98126b5ac1d24713
José Eduardo Méndez Barrios <joseeduardomendezb@gmail.com>
2020-11-02 02:30:15 -0700

    add animation.css example

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,11 @@
         <transition name="slide" type="animation">
           <div class="notification is-info" v-if="show">This is some info</div>
         </transition>
-        <transition name="fade" appear>
+        <transition
+          appear
+          enter-active-class="animated bounce"
+          leave-active-class="animated shake"
+        >
           <div class="notification is-info" v-if="show">This is some info</div>
         </transition>
       </div>
