d2d783e951153fbcd2c02b65bead61bf7c0e33ea
José Eduardo Méndez Barrios <joseeduardomendezb@gmail.com>
2020-11-02 03:53:09 -0700

    animation in js

--- src/App.vue
+++ src/App.vue
@@ -65,9 +65,10 @@
           @leave="leave"
           @after-leave="afterLeave"
           @leave-cancelled="leaveCancelled"
+          :css="false"
         >
           <div
-            style="width: 100px; height: 100px; background-color: lightgreen"
+            style="width: 300px; height: 100px; background-color: lightgreen"
             v-if="load"
           ></div>
         </transition>
@@ -82,16 +83,27 @@ export default {
     return {
       show: false,
       load: true,
-      alertAnimation: 'fade'
+      alertAnimation: 'fade',
+      elementWidth: 100
     }
   },
   methods: {
-    beforeEnter () {
+    beforeEnter (el) {
       console.log('beforeEnter')
+      this.elementWidth = 100
+      el.style.width = this.elementWidth + 'px'
     },
     enter (el, done) {
       console.log('enter')
-      done()
+      let round = 1
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth + round * 10 + 'px'
+        round++
+        if (round > 20) {
+          clearInterval(interval)
+          done()
+        }
+      }, 20)
     },
     afterEnter () {
       console.log('afterEnter')
@@ -99,12 +111,22 @@ export default {
     enterCancelled () {
       console.log('enterCancelled')
     },
-    beforeLeave () {
+    beforeLeave (el) {
       console.log('beforeLeave')
+      this.elementWidth = 300
+      el.style.width = this.elementWidth + 'px'
     },
     leave (el, done) {
       console.log('leave')
-      done()
+      let round = 1
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth - round * 10 + 'px'
+        round++
+        if (round > 20) {
+          clearInterval(interval)
+          done()
+        }
+      }, 20)
     },
     afterLeave () {
       console.log('afterLeave')
